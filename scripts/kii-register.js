690a17d1428c80c6e57761008e0894a2dd674310
Mateo <me@mateo.email>
2016-02-19 22:27:19 -0800

    please

--- scripts/kii-register.js
+++ scripts/kii-register.js
@@ -1,22 +1,24 @@
+function performRegistration() {
+    
+    var username = $("#register-name").val();
+    var password = $("#register-password").val();
+    
+    var user = KiiUser.userWithUsername(username, password);
+
+    user.register({
+        success: function(theUser) {
+            console.log("User registered!");
+            console.log(theUser);
+        },
+
+        failure: function(theUser, errorString) {
+            console.log("Error Registering: " + errorString);
+        }
+    });
+}
 $(document).ready(function() {
     // initialize the Kii SDK!
     Kii.initializeWithSite("3548f996", "245e2a0d275ff86e28e3eb0c2e4eb989", KiiSite.US);
     // bind clicks to our login/sign up methods
-    $("#register-button").click(function performRegistration() {
-        var username = $("#register-name").val();
-        var password = $("#register-password").val();
-        
-        var user = KiiUser.userWithUsername(username, password);
-
-        user.register({
-            success: function(theUser) {
-                console.log("User registered!");
-                console.log(theUser);
-            },
-
-            failure: function(theUser, errorString) {
-                console.log("Error Registering: " + errorString);
-            }
-        });
-    });
+    $("#register-button").click(performRegistration);
 });
