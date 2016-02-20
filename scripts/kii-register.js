b644b66d0de550d64f18b6fae811faa421b15883
Mateo <me@mateo.email>
2016-02-19 22:11:56 -0800

    fixing registration script

--- scripts/kii-register.js
+++ scripts/kii-register.js
@@ -6,52 +6,18 @@ function performRegistration() {
     var username = $("#register-name").val();
     var password = $("#register-password").val();
     
-    // create the user
-    try {
-        var user = KiiUser.userWithUsername(username, password);
+    var user = KiiUser.userWithUsername(username, password);
 
-        // perform the asynchronous registration, with callbacks defined
-        user.register({
-        
-            // callback for successful registration
-            success: function(theAuthedUser) {
-            
-                // show the list of objects
-                $.mobile.changePage("list.html");
-                
-                // hide the loading animation
-                $.mobile.hidePageLoadingMsg();  
-                
-                // tell the console
-                Kii.logger("User registered: " + user);
-            },
-            
-            // callback for failed registration
-            failure: function(theUser, anErrorString) {
+    user.register({
+        success: function(theUser) {
+            console.log("User registered!");
+            console.log(theUser);
+        },
 
-                // hide the loading animation
-                $.mobile.hidePageLoadingMsg();  
-                
-                // tell the user
-                alert("Unable to register: " + anErrorString);
-                
-                // tell the console
-                Kii.logger("Unable to register user: " + anErrorString);
-            }
-        });
-
-    } catch(e) {
-
-        // hide the loading animation
-        $.mobile.hidePageLoadingMsg();  
-        
-        // tell the user
-        alert("Unable to register: " + e.message);
-        
-        // tell the console
-        Kii.logger("Unable to register user: " + e.message);
-    }
-
+        failure: function(theUser, errorString) {
+            console.log("Error Registering: " + errorString);
+        }
+    });
 }
 $(document).ready(function() {
     // initialize the Kii SDK!
